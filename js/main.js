98362b082a567eb46c217814b8bf4f77a21abf3f
Leandro Matias Cabrera <lcabrera@entradauno.com>
2023-01-28 11:44:01 -0300

    correcciones menores

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
-$('document').ready( ()=> {
-    type('Cabrera Leandro', 'title').then((r) => {
-        type('Curriculum Vitae.', 'subtitle');
-    });
-    
-    timeline($('.timeline'), {
-        verticalStartPosition: 'left',
-    });
-    
-    $('circle').hover(
-        (r)=>{
-            $(r.target).attr("stroke-width","10");
-        }, 
-        (r)=>{ 
-            $(r.target).attr("stroke-width","5");
-        } 
-    );
+$("document").ready(() => {
+  type("Cabrera Leandro", "title").then((r) => {
+    type("Curriculum Vitae.", "subtitle");
+  });
+
+  timeline($(".timeline"), {
+    verticalStartPosition: "left",
+  });
+
+  $("circle").hover(
+    (r) => {
+      $(r.target).attr("stroke-width", "10");
+    },
+    (r) => {
+      $(r.target).attr("stroke-width", "5");
+    }
+  );
 });
 
 async function type(title, domId) {
-    let titleDom = $('#'+domId);
-    for (let i = 0; i <= title.length; i++) {
-        if (i == title.length) {
-            console.log(title, ' TERMINA SECUENCIA TYPE');
-            return 'finish';
-        }
-        titleDom.text(titleDom.html() + title[i]);
-        await delay(50);
+  let titleDom = $("#" + domId);
+  for (let i = 0; i <= title.length; i++) {
+    if (i == title.length) {
+      console.log(title, " TERMINA SECUENCIA TYPE");
+      return "finish";
     }
+    titleDom.text(titleDom.html() + title[i]);
+    await delay(50);
+  }
 }
+
 function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
